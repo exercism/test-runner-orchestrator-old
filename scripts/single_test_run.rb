3a56f4e54d0c669120f2c4dc272f466cae6c289b
Charles Care <c.p.care@gmail.com>
2019-11-18 21:41:45 +0000

    Working test-run with a real endpoint

--- scripts/single_test_run.rb
+++ scripts/single_test_run.rb
@@ -8,7 +8,7 @@ s3_uri = "s3://exercism-submissions/production/submissions/96"
 
 container_version = "git-b6ea39ccb2dd04e0b047b25c691b17d6e6b44cfb"
 
-client = PipelineClient.new
+client = PipelineClient.new(address: "tcp://analysis-router:5555")
 test_runner = TestRunner.new(client, "ruby")
 test_runner.configure_version(container_version)
 data = test_runner.run_tests("two-fer", s3_uri)
