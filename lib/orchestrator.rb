b16f94f06700289021a117e08dceecf56233a40e
Jeremy Walker <jez.walker@gmail.com>
2019-12-06 13:00:05 +0900

    Reorder tracks

--- lib/orchestrator.rb
+++ lib/orchestrator.rb
@@ -20,13 +20,15 @@ module Orchestrator
   # Todo build this from JSON
   # Tracks with:
   #  - timeouts in milliseconds
+  #  - container versions set to whatever is currently deployed
   TRACKS = Concurrent::Map.new
-  TRACKS[:ruby] = {timeout: 3_000, container_version: "git-da694960c8c8d5c27c50885966a4301c050ce83a"}
-  TRACKS[:rust] = {timeout: 10_000, container_version: "git-91aca5f26365595d76bf60b114a6eeefc3d416a5"}
   TRACKS[:csharp] = {timeout: 10_000, container_version: "git-6e7b53c8572dd9475a2108726022952b9de50fde"}
   TRACKS[:elixir] = {timeout: 10_000, container_version: "git-a8c7b8e5c1881792c4169e816c7b737b2ba7305c"}
   TRACKS[:python] = {timeout: 10_000, container_version: "git-777031cbe192bbc567fd5b5253db4b0545621e6c"}
+  TRACKS[:ruby]   = {timeout: 3_000,  container_version: "git-da694960c8c8d5c27c50885966a4301c050ce83a"}
+  TRACKS[:rust]   = {timeout: 10_000, container_version: "git-91aca5f26365595d76bf60b114a6eeefc3d416a5"}
 
+  # A threadpool per track
   THREADPOOLS = Concurrent::Map.new
   TRACKS.keys.each do |track|
     THREADPOOLS[track] = PipelineClientThreadPool.new(track)
