d72643e958e38504d53b16cb096c9b4f86fe1a66
Charles Care <c.p.care@gmail.com>
2019-12-13 16:03:45 +0000

    Update Python target version

--- lib/orchestrator.rb
+++ lib/orchestrator.rb
@@ -24,7 +24,7 @@ module Orchestrator
   TRACKS = Concurrent::Map.new
   TRACKS[:csharp] = {timeout: 10_000, container_version: "git-f00ff8e2378b833cbf5f27ff4e85f0523dcba036"}
   TRACKS[:elixir] = {timeout: 10_000, container_version: "git-a8c7b8e5c1881792c4169e816c7b737b2ba7305c"}
-  TRACKS[:python] = {timeout: 10_000, container_version: "git-777031cbe192bbc567fd5b5253db4b0545621e6c"}
+  TRACKS[:python] = {timeout: 10_000, container_version: "git-da816aea774f7f54fb02d9f64ce8ebefb75d46c8"}
   TRACKS[:ruby]   = {timeout: 3_000,  container_version: "git-41285e7491989b9dd334e29be29d0c1c120dd866"}
   TRACKS[:rust]   = {timeout: 10_000, container_version: "git-91aca5f26365595d76bf60b114a6eeefc3d416a5"}
   TRACKS[:javascript] = {timeout: 10_000, container_version: "git-d5402c2f9e1d4b01517675680fa21201c9344f91"}
